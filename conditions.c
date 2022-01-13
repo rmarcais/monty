ec46316e1310102266a3033f72a144f8a62ea5cc
Rémi Marçais <remi.marcais87@gmail.com>
2022-01-13 09:37:28 +0100

    Fix Betty warnings

--- conditions.c
+++ conditions.c
@@ -1,4 +1,10 @@
 #include "monty.h"
+/**
+ * condition - tests conditions
+ * @stack: The stack
+ * @i: the iteration in the for loop of parseline.c
+ * Return: 0 or 1
+ */
 int condition(stack_t **stack, int i)
 {
 	if (((i == 2 || i == 3 || i == 4 || i == 5 || i == 6) && *stack == NULL)
